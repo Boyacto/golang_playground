ecff576bb827a2eaa9e7f9f14bda18fbab4e895f
Boyacto <bohyeoncha2000@gmail.com>
2025-09-29 22:51:32 -0600

    Add Push and Pop methods to Stack implementation in main.go

--- Tarea1_A01023804/main.go
+++ Tarea1_A01023804/main.go
@@ -9,6 +9,13 @@ type Stack[T any] struct {
 	data []T
 }
 
+func (s *Stack[T]) Push(v T) {
+	//tengo que encontrar de alguna forma como append para agregar el elemento
+}
+func (s *Stack[T]) Pop() (T, bool) {
+	//tengo que checar si esta en 0 y si no está vacio quitar la longitud y regresar el valor que hice pop
+}
+
 // ===== Queue (FIFO) =====
 type Queue[T any] struct {
 	data []T
