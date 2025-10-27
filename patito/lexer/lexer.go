cf1e5434ba7fe053437b074259670fb5f4538ba9
Boyacto <bohyeoncha2000@gmail.com>
2025-10-26 22:20:25 -0600

    Add whitespace handling in lexer: implement skipWhitespace function to improve token parsing by ignoring whitespace characters.

--- patito/lexer/lexer.go
+++ patito/lexer/lexer.go
@@ -28,6 +28,8 @@ func New(input string) *Lexer {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -62,6 +64,12 @@ func (l *Lexer) NextToken() token.Token {
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func (l *Lexer) readIdentifier() string {
