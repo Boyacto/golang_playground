310af51885e43190022b7c254037531936154e53
Boyacto <bohyeoncha2000@gmail.com>
2025-10-26 19:36:35 -0600

    Refactor lexer package: update import path for token and reorganize New function

--- patito/lexer/lexer.go
+++ patito/lexer/lexer.go
@@ -1,16 +1,12 @@
 package lexer
 
+import "./token"
+
 type Lexer struct {
 	input        string
 	currentIndex int  // current index in input (points to current char)
 	nextIndex    int  // current reading index in input (after current char)
 	ch           byte // current char under examination
-}
-
-func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
-	return l
 }
 
 func (l *Lexer) readChar() {
@@ -21,4 +17,43 @@ func (l *Lexer) readChar() {
 	}
 	l.currentIndex = l.nextIndex
 	l.nextIndex += 1
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
